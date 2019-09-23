80e68f3795df445c93380c6070ce230e5ab558a4
Arnaud FRANCOIS <arnaud.fr21@gmail.com>
2019-09-23 17:43:44 +0200

    AJout 2 netbeans

--- Panier/Panier/src/main/java/fr/ufrsciencestech/panier/Panier.java
+++ Panier/Panier/src/main/java/fr/ufrsciencestech/panier/Panier.java
@@ -53,10 +53,47 @@ public class Panier
 		return false;
 				
 	}
+        public void retire()
+	{
+		this.liste.remove(this.liste.size()-1);
+	}
+
+	public double getPrix()
+	{
+		double total=0;
+		for(int i=0; i<this.getSize();i++)
+		{
+			total= total+this.liste.get(i).getPrix();
+		}
+		return total;
+        }
+        
+        public String toString(){
+            String res ="";
+            double total=0;
+            for(int i=0; i<this.getSize();i++)
+            {
+            	res+= "element "+i+" : origine "+this.liste.get(i).getOrigine()+" et prix :"+this.liste.get(i).getPrix()+"$\n";
+            }
+            return res;
+            
+        }
+        
+        public void boycotteOrigine(String s)
+        {
+            for(int i=0;i<this.liste.size();i++)
+            {
+                if(this.liste.get(i).getOrigine().equals(s) )
+                    this.liste.remove(i);
+            }
+            
+            
+            
+        }
 
 	public static void main(String args[]) throws Exception
 	{
-		Panier p = new Panier(5);
+            Panier p = new Panier(5);
 		
 
 		for(int i =0; i<8;i++){
@@ -65,13 +102,26 @@ public class Panier
 			if(p.estPlein())
 				System.out.println("le panier est plien");
 			if(p.ajoute(new Orange(9.99,"france")))
-			System.out.println("il y a "+p.getSize()+" element dans le panier de taille : "+p.getTaille());
+			System.out.println("il y a "+p.getSize()+" element dans le panier de taille : "+p.getTaille()+"est le prix est de "+p.getPrix());
 			else{
 				System.out.println("le panier est plien");
 			}
 
 		}
-
+		p.retire();
+                System.out.println("element retirer il y a "+p.getSize()+" element dans le panier de taille : "+p.getTaille()+"est le prix est de "+p.getPrix());
+                System.out.println(p.toString());
+                
+                Panier p2 = new Panier(5);
+                
+                p2.ajoute(new Orange(0.80,"France"));
+                p2.ajoute(new Orange(0.80,"Espagne"));
+                p2.ajoute(new Orange(0.90,"Floride"));
+                p2.boycotteOrigine("France");
+                
+                System.out.println("----------------------");
+                System.out.println(p2.toString());
+                
 	}
 
 }
